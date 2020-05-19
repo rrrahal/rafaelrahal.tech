a8d6a040754ac16b8c9c1aa4db00019b19699a44
Rafael Rahal <rafaelrahal@Rafaels-MacBook-Pro.local>
2020-05-19 00:49:46 -0300

    v0 is finally done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+
 import './App.css';
 
 function App() {
+  const phrases = [
+    "I'm a Computer Engineering Student",
+    "I like turning code into real stuff",
+    "I also enjoy web development very much",
+    "(Especially front-end)",
+    "I'm always trying to learn new things",
+    "Find me and say Hello (:"
+  ];
+  const [phraseIndex, setPhraseIndex] = useState(0)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setPhraseIndex((phraseIndex + 1) % phrases.length);
+    }, 2800);
+  }, [phraseIndex])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content-wrapper">
+        <div className="greeting">
+            Hi, I'm Rafael!
+        </div>
+        <div className="titles-container">
+          <div className="title">
+           {phrases[phraseIndex]}
+          </div>
+        </div>
+        <FontAwesomeIcon icon={faCoffee} />
+      </div>
     </div>
   );
 }
