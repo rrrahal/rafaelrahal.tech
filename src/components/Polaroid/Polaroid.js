063bd75e87c3b82829921f15b736fef93edf0d33
Rafael Rahal <rrrahal@gmail.com>
2021-07-03 17:30:23 +0100

    add animation

--- src/components/Polaroid/Polaroid.js
+++ src/components/Polaroid/Polaroid.js
@@ -6,7 +6,7 @@ export const Polaroid = () => {
     return (
     <div className="polaroid">
       <div className="picture-container">
-        <img src="https://i.ibb.co/Gvp7t6H/porto-retrato.jpg" alt="my-polaroid-pic" className="picture" />
+        <img src="https://i.ibb.co/Gvp7t6H/porto-retrato.jpg" alt="my-polaroid-pic" className="picture fade-in" />
       </div>
       <p className="caption">Rafael Rahal</p>
     </div>
