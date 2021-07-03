f3f9ab47f9c2e31bb9b5e2556b284601865267ee
Rafael Rahal <rrrahal@gmail.com>
2021-07-03 13:20:20 +0100

    paper sheet first version

--- src/components/PaperSheet/PaperSheet.js
+++ src/components/PaperSheet/PaperSheet.js
@@ -5,7 +5,22 @@ import './PaperSheet.css'
 export const PaperSheet = () => {
   return (
     <div className="paper-container">
-      yo
+      <div className="paper">
+        <div className="title" role="textbox">
+            Lorem ipsum dolor sit amet.
+        </div>
+        <div className="first-paragraph" role="textbox">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Illo et cumque animi cum delectus adipisci illum laboriosam explicabo, voluptatem ab?
+        </div>
+        <div className="second-paragraph" role="textbox">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Illo et cumque animi cum delectus adipisci illum laboriosam explicabo, voluptatem ab?
+        </div>
+        <div className="links" role="textbox">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </div>
+      </div>
     </div>
   )
 
