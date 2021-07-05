5b2a33fecd4a9cb94e99934c6599c7a0d034e9b8
Rafael Rahal <rrrahal@gmail.com>
2021-07-05 21:39:54 +0100

    desktop version first version

--- src/components/Polaroid/Polaroid.js
+++ src/components/Polaroid/Polaroid.js
@@ -4,11 +4,13 @@ import './Polaroid.css'
 
 export const Polaroid = () => {
     return (
-    <div className="polaroid">
+    <div className="polaroid-container">
+      <div className="polaroid">
       <div className="picture-container">
         <img src="https://i.ibb.co/Gvp7t6H/porto-retrato.jpg" alt="my-polaroid-pic" className="picture fade-in" />
       </div>
       <p className="caption">Rafael Rahal</p>
+      </div>
     </div>
     )
 }
