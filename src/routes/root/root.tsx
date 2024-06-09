68c2041cde0a74b531e0cd8785564d9a163cd4f5
Rafael Rahal <rrrahal@gmail.com>
2024-06-09 14:43:47 +0100

    add icons to navbar

--- src/routes/root/root.tsx
+++ src/routes/root/root.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router-dom";
+import { MoonIcon, SunIcon } from '@radix-ui/react-icons'
 
 export default function Root() {
     return (
-    <div className="container flex flex-col">
-        <nav className="container flex  justify-between">
+    <div className="container flex flex-col p-4 items-center">
+        <nav className="container flex  justify-between max-w-[60%] p-2 bg-slate-200">
             <h2>Rafael Rahal</h2>
-            <div className="flex space-x-5">
+            <div className="flex space-x-5 items-center">
                 <h2> About </h2>
                 <h2> Blog </h2>
+                <MoonIcon />
+                <SunIcon />
             </div>
         </nav>
         <Outlet />
