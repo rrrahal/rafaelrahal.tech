5e522da92b840f02aadfebec503e01571a274962
Rafael Rahal <rrrahal@gmail.com>
2024-06-10 16:57:35 +0100

    add catpucin for dark and light themes

--- src/routes/root/root.tsx
+++ src/routes/root/root.tsx
@@ -1,10 +1,16 @@
+import { useState } from 'react'
 import { Outlet, Link } from "react-router-dom";
 import { MoonIcon, SunIcon } from '@radix-ui/react-icons'
 
 export default function Root() {
+    const [theme, setTheme] = useState("ctp-latte")
+    const icon = theme === "ctp-latte" ?
+        <MoonIcon onClick={() => setTheme("ctp-mocha")} /> :
+        <SunIcon onClick={() => setTheme("ctp-latte")} />
+
     return (
-        <main  className="flex flex-col items-center">
-            <header className="flex flex-col items-center justify-center p-2 bg-slate-200 w-full">
+        <main  className={`flex flex-col items-center ${theme} bg-ctp-base text-ctp-text min-h-screen`}>
+            <header className="flex flex-col items-center justify-center p-2 bg-ctp-crust w-full">
                 <nav className="flex justify-between max-w-[60%] w-full">
                     <Link to="/"><h2>Rafael Rahal</h2></Link>
                     <div className="flex space-x-5 items-center">
@@ -14,12 +20,11 @@ export default function Root() {
                         <Link to={"/blog"}>
                             <h2> Blog </h2>
                         </Link>
-                        <MoonIcon />
-                        <SunIcon />
+                        {icon}
                     </div>
                 </nav>
             </header>
-        <section className="w-full  max-w-[60%]">
+        <section className="w-full  max-w-[60%] text-ctp-text">
             <Outlet />
         </section>
         </main>
