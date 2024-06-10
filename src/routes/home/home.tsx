0ad8b188c158daa4217855a851eb6e6bcc7b3a8d
Rafael Rahal <rrrahal@gmail.com>
2024-06-10 16:15:28 +0100

    change navbar styles

--- src/routes/home/home.tsx
+++ src/routes/home/home.tsx
@@ -1,10 +1,10 @@
 export default () => {
     return (
-        <>
-            <h2> Hello! I'm a Software Engineer</h2>
+        <article className="flex items-center justify-center flex-col p-6">
+            <h2 className="bold p-4"> Hello! I'm a Software Engineer</h2>
             <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit.
                 Distinctio tempore aliquam molestias sint atque, natus rem sequi ipsum. Laborum, excepturi.
             </p>
-        </>
+        </article>
     )
 }
--- src/routes/root/root.tsx
+++ src/routes/root/root.tsx
@@ -3,21 +3,25 @@ import { MoonIcon, SunIcon } from '@radix-ui/react-icons'
 
 export default function Root() {
     return (
-    <div className="container flex flex-col p-4 items-center">
-        <nav className="container flex  justify-between max-w-[60%] p-2 bg-slate-200">
-            <Link to="/"><h2>Rafael Rahal</h2></Link>
-            <div className="flex space-x-5 items-center">
-                <Link to="/about">
-                    <h2> About </h2>
-                </Link>
-                <Link to={"/blog"}>
-                    <h2> Blog </h2>
-                </Link>
-                <MoonIcon />
-                <SunIcon />
-            </div>
-        </nav>
-        <Outlet />
-    </ div>
+        <main  className="flex flex-col items-center">
+            <header className="flex flex-col items-center justify-center p-2 bg-slate-200 w-full">
+                <nav className="flex justify-between max-w-[60%] w-full">
+                    <Link to="/"><h2>Rafael Rahal</h2></Link>
+                    <div className="flex space-x-5 items-center">
+                        <Link to="/about">
+                            <h2> About </h2>
+                        </Link>
+                        <Link to={"/blog"}>
+                            <h2> Blog </h2>
+                        </Link>
+                        <MoonIcon />
+                        <SunIcon />
+                    </div>
+                </nav>
+            </header>
+        <section className="w-full  max-w-[60%]">
+            <Outlet />
+        </section>
+        </main>
     )
 }
