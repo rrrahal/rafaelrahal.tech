d16f39e26a6ca005e9107fd6798f7ede26ae3027
Rafael Rahal <rrrahal@gmail.com>
2024-06-23 17:06:37 +0100

    improve post Layout

--- src/routes/blog/posts-layout.tsx
+++ src/routes/blog/posts-layout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
 
 export default () => {
-  return <article className="flex flex-col justify-center items-center">{<Outlet />}</article>;
+  return <article className="flex flex-col justify-center items-center gap-4">{<Outlet />}</article>;
 };
